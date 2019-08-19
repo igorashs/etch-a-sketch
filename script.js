f6f29d2f05e9f43a6f52889c6a1c3241e7a6cf2d
igorashs <igorashsirbu@gmail.com>
2019-08-19 03:25:36 +0300

    Add basic function (black)

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ function makeNewGrid(count, size) {
 
   for (let i = 0; i < count * count; i++) {
     let element = document.createElement('div');
-    element.classList.add('grid-element');
+    element.style.backgroundColor = WHITE;
     gridContainer.appendChild(element);
   }
   const etchContainer = document.querySelector('.etch-container');
 
   etchContainer.insertBefore(gridContainer, etchContainer.lastElementChild);
+
+  gridContainer.addEventListener('mouseover', (e) => {
+    if (!e.target.classList.contains('grid-container')) {
+      let rgb = e.target.style.backgroundColor.match(REGX_RGB);
+      let hslv = rgbToHsl(...rgb);
+      if (hslv[2] >= 10) hslv[2] -= 10;
+      let hsl = `hsl(${hslv[0]}, ${hslv[1]}%, ${hslv[2]}%)`;
+      e.target.style.backgroundColor = hsl;
+    }
+  });
 }
 
 function resetGrid() {}
@@ -38,12 +48,49 @@ makeNewGridBtn.addEventListener('click', (e) => {
   makeNewGrid(currentCount, GRID_SIZE);
 });
 
-const WHITE = 'hsla(0, 50%, 100%, 1)';
-const BLACK = 'hsla(0, 0%, 90%, 1)';
+const WHITE = 'hsl(0, 50%, 100%)';
+const BLACK = 'hsl(0, 0%, 90%)';
+const REGX_RGB = /\d+/g;
 
 const GRID_SIZE = 512;
 let elementsCount = 16;
 
-// hsla(270, 50%, 90%, 1) random purple 0-360 random 90% will decrease each hover
+let currentColor = BLACK;
 
+// hsla(270, 50%, 90%) random purple 0-360 random 90% will decrease each hover
+
+// init add a fricking fucntion for this late
 makeNewGrid(elementsCount, GRID_SIZE);
+
+function rgbToHsl(r, g, b) {
+  (r /= 255), (g /= 255), (b /= 255);
+
+  let max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+
+    h /= 6;
+  }
+
+  return [+(h * 360).toFixed(), +(s * 100).toFixed(), +(l * 100).toFixed()];
+}
